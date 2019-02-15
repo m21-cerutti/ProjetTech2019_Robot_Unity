2f3a07275060aaaf3a2989a53f2c70b5d52415d4
mcerutti <marc.cerutti@etu.u-bordeaux.fr>
2019-02-15 18:18:27 +0100

    Travail start

---
+++ TestRobot/Assets/ManualInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputRobot))]
+public class ManualInput : MonoBehaviour
+{
+    //Manual input
+    public float Horizontal
+    {
+        get { return Input.GetAxis("Horizontal"); }
+    }
+
+    public float Vertical
+    {
+        get { return Input.GetAxis("Vertical"); }
+    }
+
+    public bool A
+    {
+        get { return Input.GetButtonDown("LeftA"); }
+    }
+
+}
---
+++ TestRobot/Assets/StereoCamera.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class StereoCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Camera left_cam;
+    [SerializeField]
+    private Camera right_cam;
+    [SerializeField]
+    private float distance_cam = 0.5f;
+
+    void Start()
+    {
+        SetCamerasDistance();
+    }
+
+    public void SetCamerasDistance()
+    {
+        //Distance beetween eyes
+        right_cam.transform.localPosition = new Vector3(distance_cam, 0);
+        left_cam.transform.localPosition = new Vector3(-distance_cam, 0);
+
+    }
+
+    private byte[] GetCameraImage(Camera cam)
+    {
+
+        RenderTexture currentRT = RenderTexture.active;
+        RenderTexture.active = cam.targetTexture;
+
+        cam.Render();
+
+        Texture2D Image = new Texture2D(cam.activeTexture.width, cam.activeTexture.height);
+        Image.ReadPixels(new Rect(0, 0, cam.activeTexture.width, cam.activeTexture.height), 0, 0);
+        Image.Apply();
+        RenderTexture.active = currentRT;
+
+        byte[] bytes = Image.EncodeToPNG();
+        Destroy(Image);
+
+        return bytes;
+    }
+
+    public byte[] GetCameraLeft()
+    {
+        Debug.Log("GetCameraLeft");
+        return GetCameraImage(left_cam);
+    }
+
+    public byte[] GetCameraRight()
+    {
+        Debug.Log("GetCameraRight");
+        return GetCameraImage(right_cam);
+    }
+
+}
+
+[CustomEditor(typeof(StereoCamera))]
+public class LevelScriptEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        StereoCamera myTarget = (StereoCamera)target;
+
+        if (GUILayout.Button("Calculate distance cameras"))
+        {
+            myTarget.SetCamerasDistance();
+        }
+
+        if (GUILayout.Button("GetCameraLeft"))
+        {
+            myTarget.GetCameraLeft();
+        }
+
+        if (GUILayout.Button("GetCameraRight"))
+        {
+            myTarget.GetCameraRight();
+        }
+    }
+}
