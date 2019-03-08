07e4157fb0b20ecb06bb9522ac3a345a248dbc69
Unknown <m21.cerutti@gmail.com>
2019-03-08 18:19:21 +0100

    Programe à tester server

--- TestRobot/Assets/Script/Client.cs
+++ TestRobot/Assets/Script/Client.cs
@@ -10,6 +10,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Configuration;
 using System.Text;
+using System.Linq;
 
 [RequireComponent(typeof(StereoCamera))]
 public class Client : MonoBehaviour
@@ -51,146 +52,188 @@ public class Client : MonoBehaviour
 
     public string adressIp = "192.168.0.40";
     public int port = 5260;
-    Socket sender;
-    Socket handler;
+	TcpClient client;
+	NetworkStream sender;
+	int size_command;
 
-    void echoCommand(string message)
+	void inEchoCommand(byte[] data)
     {
-        Debug.Log("Resend: " + message);
-        sender.Send(Encoding.ASCII.GetBytes("r_echo;" + message+ "<EOF>"));
+		string message = Encoding.ASCII.GetString(data);
+		Debug.Log("Resend: " + message);
+		sendCommand(Encoding.ASCII.GetBytes("r;" + message));
     }
 
-    private void rechoCommand(string data)
+    private void inRechoCommand(byte[] data)
     {
-        Debug.Log("Return echo complete: " + data);
+		string message = Encoding.ASCII.GetString(data);
+		Debug.Log("Return echo complete: " + message);
     }
 
-    void refreshCommand(string number)
+    void inRefreshCommand(byte[] data)
     {
-        int value;
-        try
-        {
-            value = int.Parse(number);
-            Debug.Log("New value refresh :" + value);
-        }
-        catch(Exception e)
-        {
-            Debug.LogError(e.ToString());
-        }
+		int value = (int)BitConverter.ToInt32(data,0);
+        Debug.Log("New value refresh :" + value);
+		cameras.refreshTime = value;
     }
 
-    void sendLargeFile(string identifier, byte[] data)
-    {
-        IList<ArraySegment<byte>> cmd = new List<ArraySegment<byte>>();
-        cmd.Add(new ArraySegment<byte>(Encoding.ASCII.GetBytes(identifier + ":"+data.Length+";")));
-        cmd.Add(new ArraySegment<byte>(data));
-        cmd.Add(new ArraySegment<byte>(Encoding.ASCII.GetBytes("<EOF>")));
-        int bytesSentStart = sender.Send(cmd);
-    }
+	void sendCommand(byte[] data)
+	{
+		int size_message = sizeof(Int32) + data.Length;
+		byte[] message = new byte[size_message];
+		BitConverter.GetBytes((Int32)data.Length).CopyTo(message, 0);
+		data.CopyTo(message, sizeof(Int32));
+		sender.Write(message, 0, size_message);
+	}
 
-    void sendCamerasCommand()
+	private byte[] Combine(params byte[][] arrays)
+	{
+		byte[] rv = new byte[arrays.Sum(a => a.Length)];
+		int offset = 0;
+		foreach (byte[] array in arrays)
+		{
+			System.Buffer.BlockCopy(array, 0, rv, offset, array.Length);
+			offset += array.Length;
+		}
+		return rv;
+	}
+
+	void sendCamerasCommand()
     {
         byte[][] images = cameras.getCamerasImages();
-        int bytesSentStart = sender.Send(Encoding.ASCII.GetBytes("stereo;"));
-        sendLargeFile("left", images[0]);
-        sendLargeFile("right", images[1]);
-    }
 
-    private void receiveImageCommand(string cmd, string data, byte[] bytes, int nbread)
-    {
-        Debug.Log(cmd);
-        Debug.Log(data);
-    }
+		byte[] command = Combine(
+			Encoding.ASCII.GetBytes("s"),
+			BitConverter.GetBytes((Int32)images[0].Length),
+			images[0],
+			BitConverter.GetBytes((Int32)images[1].Length),
+			images[1]);
 
-    int parseCommand(byte[] bytes, int nbread)
-    {
-        string buff = Encoding.ASCII.GetString(bytes, 0, nbread);
+		sendCommand(command);
+	}
 
-        int endCommand = 0;
-        while((endCommand = buff.IndexOf("<EOF>")) > 0)
-        {
-            //Debug.Log("buff debug = " + buff);
+	void sendRefreshCommand(int refresh_time)
+	{
+		byte[] command = Combine(
+			Encoding.ASCII.GetBytes("t;"),
+			BitConverter.GetBytes(refresh_time));
 
-            string cmd = buff.Substring(0, endCommand);
-            Debug.Log("Command debug = " + cmd);
+		sendCommand(command);
+	}
 
-            buff = buff.Remove(0, endCommand + "<EOF>".Length);
-            //Debug.Log("newbuff debug = " + buff);
 
-            if (cmd.Equals(""))
-                throw new Exception("Server shutdown.");
+	int parseCommand(byte[] data)
+	{
+		string cmd = Encoding.ASCII.GetString(data,0,2);
+		Debug.Log("Command read = " + cmd);
+		//send echo
+		if (cmd.StartsWith("e;"))
+		{
+			inEchoCommand(data);
+		}
+		//send recho
+		else if (cmd.StartsWith("r;"))
+		{
+			inRechoCommand(data);
+		}
+		//send camera
+		else if (cmd.StartsWith("s;"))
+		{
+			sendCamerasCommand();
+		}
+		//send time refresh
+		else if (cmd.StartsWith("t;"))
+		{
+			inRefreshCommand(data);
+		}
+		else
+		{
+			throw new Exception("Illegal instruction.");
+		}
 
-            int beginData = cmd.IndexOf(";")+1;
-            string data = cmd.Substring(beginData);
-            Debug.Log("Data debug = "+ data);
-
-            if (cmd.StartsWith("echo"))
-            {
-                echoCommand(data);
-            }
-            else if (cmd.StartsWith("r_echo"))
-            {
-                rechoCommand(data);
-            }
-            else if (cmd.StartsWith("req_img"))
-            {
-                sendCamerasCommand();
-            }
-            else if (cmd.StartsWith("img"))
-            {
-                receiveImageCommand(cmd, data, bytes, nbread);
-            }
-            else if (cmd.StartsWith("refresh"))
-            {
-                refreshCommand(data);
-            }
-            else
-            {
-                throw new Exception("Illegal instruction.");
-            }
-        }
-        return 0;
-    }
+		return 0;
+	}
 
     void networkCode()
     {
-        // Data buffer for incoming data.
-        byte[] bytes = new byte[1024];
 
-        // host running the application.
-        Debug.Log("Locals Ip " + getIPAddress().ToString());
-        Debug.Log("Try to connect to " + adressIp + ":" + port);
-        IPAddress[] ipArray = Dns.GetHostAddresses(adressIp);
-
-        IPEndPoint remoteEndPoint = new IPEndPoint(ipArray[0], port);
-        // Create a TCP/IP socket.
-        sender = new Socket(ipArray[0].AddressFamily,
-            SocketType.Stream, ProtocolType.Tcp);
+		TcpClient client = new TcpClient();
 
         try
         {
-            sender.Connect(remoteEndPoint);
-            Debug.Log("Socket connected to " + sender.RemoteEndPoint.ToString());
+			Debug.Log("Try to connect to " + adressIp + ":" + port);
+			Debug.Log("Connecting...");
 
-            //Send refresh req
-            int bytesSentStart = sender.Send(Encoding.ASCII.GetBytes("start;" + cameras.refreshTime.ToString() + ";<EOF>"));
+			client.Connect(adressIp, port);
 
-            while (keepReading)
-            {
-                // Receive the response from the remote device. Block.  
-                int nbread = sender.Receive(bytes);
+			Debug.Log("Connected.");
 
-                //Parse command
-                parseCommand(bytes, nbread);
-                
-                if (nbread == 0)
-                {
-                    keepReading = false;
-                }
+			int bufferSize = client.ReceiveBufferSize;
+			Debug.Log("Size Buffer : " + bufferSize + " bytes.");
+			Debug.Log("Time Out : " + client.ReceiveTimeout + " bytes.");
+
+
+			using (sender = client.GetStream())
+			{
+				//Send refresh req start
+				sendRefreshCommand((int)cameras.refreshTime);
+				Debug.Log("Refresh send.");
+
+
+				while (keepReading && client.Client.Connected)
+				{
+					// Data buffer for incoming data.
+					byte[] buf = new byte[bufferSize];
+					int len = 0;
+					int size_command = 0;
+
+					if (sender.CanRead)
+					{
+						int read;
+						while (len < sizeof(Int32))
+						{
+							read = sender.Read(buf, len, buf.Length - len);
+							len += read;
+						}
+
+						//Buff have size package
+						size_command = BitConverter.ToInt32(buf, 0);
+						Debug.Log("Size package : " + size_command + " bytes.");
+						if (size_command > buf.Length)
+						{
+							bufferSize = size_command + 1;
+							Debug.Log("New size Buffer : " + bufferSize + " bytes.");
+							byte[] newbuf = new byte[bufferSize];
+							buf.CopyTo(newbuf, 0);
+							buf = newbuf;
+						}
+
+						Debug.Log("Transfer...");
+
+						//Loop full package
+						while ((sizeof(Int32) + size_command) > len)
+						{
+							read = sender.Read(buf, len, buf.Length - len);
+							len += read;
+							if (len >= buf.Length)
+							{
+								Debug.LogError("Buffer overflow");
+							}
+						}
+
+						Debug.Log("Package arrived.");
+						//Buff have package
+						parseCommand(buf.Skip(sizeof(Int32)).ToArray());
+						size_command = 0;
+						len = 0;
+
+					}
+				}
 			}
+			Debug.Log("End connection...");
 
-            // Release the socket.  
-            sender.Shutdown(SocketShutdown.Both);
+			// Release the socket.  
+			client.Client.Shutdown(SocketShutdown.Both);
+			client.Close();
         }
         catch (ArgumentNullException ane)
         {
@@ -213,12 +256,13 @@ public class Client : MonoBehaviour
         //stop thread
         if (SocketThread != null)
         {
-            if (sender != null && sender.Connected)
+            if (client != null && client.Connected)
             {
-                sender.Disconnect(false);
                 Debug.Log("Disconnected!");
-                sender.Close();
-            }
+				client.Client.Shutdown(SocketShutdown.Both);
+				client.Client.Close();
+				client.Close();
+			}
             SocketThread.Interrupt();
         }
     }
